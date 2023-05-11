9fa2cd72a14258e96ee394d0fee246ec07713d9f
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-05-11 14:33:47 +0800

    modify

--- 2023_05_11/app.py
+++ 2023_05_11/app.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import datasource
 
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    stock_data = datasource.get_stock_data(stockid=1503) # roberthsu 2330 (自己用1513中興電)
+    stock_data = datasource.get_stock_data(stockid=1513)  # roberthsu 2330 (自己用1513中興電)  
     return render_template("index.jinja.html",data=stock_data)
 
 
@@ -25,5 +25,15 @@ def about():
 
 @app.route("/form/",methods=['GET', 'POST'])
 def form():
+    if request.method == 'POST':
+        print(f"{request.form['username']},這是post,您好")
+        return render_template("form.jinja.html")
+    elif request.method  == 'GET':
+        return render_template("form.jinja.html")
+    
 
-    return render_template("form.jinja.html")
+
+
+
+
+    
