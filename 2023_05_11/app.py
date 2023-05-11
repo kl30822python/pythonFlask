1775b12f333d7d4a08bb6241e69e76a453dad0ae
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-05-11 09:00:32 +0800

    new_2023_05_11

---
+++ 2023_05_11/app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template
+import datasource
+
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    stock_data = datasource.get_stock_data(stockid=1503) # roberthsu 2330 (自己用1513中興電)
+    return render_template("index.jinja.html",data=stock_data)
+
+
+@app.route("/features/")
+def features():
+    
+    return render_template("features.jinja.html")
+
+@app.route("/priceing/")
+def priceing():
+    
+    return render_template("priceing.jinja.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.jinja.html")
+
+
+
+
+
+    
